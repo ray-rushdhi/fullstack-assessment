686881845bc96ce4d14b1b06bd75e1fbbfd9a606
ray-rushdhi <rayyanrushdhi@proton.me>
2026-06-16 11:31:09 +0530

    Initial commit

---
+++ backend/src/main/java/com/ray/userprofiles/UserprofilesApplication.java
@@ -0,0 +1,13 @@
+package com.ray.userprofiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserprofilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserprofilesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ray/userprofiles/UserprofilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ray.userprofiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserprofilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
